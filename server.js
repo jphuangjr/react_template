5532d54e626903fb37da463dfb72db59be527a9a
Joshua Huang <jphuangjr@gmail.com>
2017-01-09 20:50:40 -0800

    UPDATE

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
+var path = require('path');
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+var PORT = process.env.PORT || 8081;
 
-app.use(express.static('public'));
+// using webpack-dev-server and middleware in development environment
+if(process.env.NODE_ENV !== 'production') {
+	var webpackDevMiddleware = require('webpack-dev-middleware');
+	var webpackHotMiddleware = require('webpack-hot-middleware');
+	var webpack = require('webpack');
+	var config = require('./webpack.config');
+	var compiler = webpack(config);
 
-app.listen(8006, console.log("running on 8006"));
+	app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
+	app.use(webpackHotMiddleware(compiler));
+}
 
-app.get("/", function (request, response) {
-	response.sendFile(__dirname + '/index.html');
+app.use(express.static(path.join(__dirname)));
+
+app.get('*', function(request, response) {
+	response.sendFile(path.resolve(__dirname, "index.html"))
+});
+
+app.listen(PORT, function(error) {
+	if (error) {
+		console.error(error);
+	} else {
+		console.info("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+	}
 });
